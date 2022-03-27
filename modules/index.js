ff5f2b1141165ed38bca7519ebe4c84369461ca3
Kakai <kenkai24@yahoo.com>
2022-03-27 15:00:32 +0200

    added some addition and subtraction functionaliy

--- modules/index.js
+++ modules/index.js
@@ -3,8 +3,20 @@ import { resultDisplay, numbersContainer, one, two, three, four, five, six, seve
 resultDisplay.textContent = '';
 
 let numsArray = [];
+let operandArray = [];
+let additionArray = [];
+let subtractionArray = [];
+let multiplicationArray = [];
+let divisionArray = [];
+let storedResult = [];
 
-function addition(args) {
+/*function addition(args) {
+    let sum = 0;
+    [...args].forEach(item => sum += item)
+    return sum;
+};*/
+
+function addition(...args) {
     let sum = 0;
     [...args].forEach(item => sum += item)
     return sum;
@@ -12,29 +24,64 @@ function addition(args) {
 
 function subtraction(args) {
     let difference = [...args][0];
-
 }
 
 [...allNumbers].forEach(number => {
     number.addEventListener('click', function() {
         numsArray.push(parseInt(number.textContent));
         resultDisplay.textContent = resultDisplay.textContent + number.textContent;
+        console.log(numsArray);
     })
 });
 
 [...allOrange].forEach(operand => {
     operand.addEventListener('click', function() {
         //numsArray.push(parseInt(operand.textContent));
+        operandArray.push(operand.textContent);
         resultDisplay.textContent = resultDisplay.textContent + operand.textContent;
+        console.log(operandArray);
     })
 });
 
+/*equals.addEventListener('click', function() {
+    let totalSum = 0;
+    //resultDisplay.textContent = addition(numsArray);
+    for (let i = 0; i < operandArray.length; i++) {
+        if (operandArray[i] === '+') {
+            let sum = addition(numsArray[i], numsArray[i+1]);
+            if (typeof storedResult[0] === undefined) {
+                storedResult.push(sum);
+            } else {
+                storedResult.splice(0, 1, ) storedResult[0] + sum;
+            }
+            
+            console.log(storedResult);
+            resultDisplay.textContent = resultDisplay.textContent + sum;
+        } else if (operandArray[i] !== '+') {
+            resultDisplay.textContent = resultDisplay.textContent;
+        } // giving incorrect values bc prev result needs to be stored!
+    }
+});*/
+
 equals.addEventListener('click', function() {
-    resultDisplay.textContent = addition(numsArray);
+    let sum = numsArray.reduce((accumulated, currValue, currIndex, array) => {
+        if (currIndex === 0) {
+            return currValue;
+        } else {
+            if (operandArray[currIndex -1] === '+') {
+                return accumulated + currValue;
+            } else if (operandArray[currIndex -1] === '-') {
+                return accumulated - currValue;
+            }
+        }
+    }, 0)
+    resultDisplay.textContent = resultDisplay.textContent + sum;
 });
+
 
 AC.addEventListener('click', function() {
     numsArray.splice(0, numsArray.length);
+    operandArray.splice(0, operandArray.length);
     resultDisplay.textContent = '';
 });
 
